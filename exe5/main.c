ff82fc2c0b084a87f040f57b3ef8092f6f7d08e7
Vitor Katakura <7vitorgomez@gmail.com>
2025-02-10 17:08:32 -0300

    exercício 5

--- exe5/main.c
+++ exe5/main.c
@@ -21,10 +21,12 @@ int main() {
     while (true) {
 
         if (!gpio_get(BTN_PIN)) {
+            sleep_ms(500);
             printf("Botao 1: %d\n", cnt_1++);
         }
 
         if (!gpio_get(BTN_PIN_2)) {
+            sleep_ms(500);
             printf("Botao 2: %d\n", cnt_2++);
         }
     }
